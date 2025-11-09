48bb33629b8231150e6c0d41bdab4399288987e1
taboubiroua <rouataboubi60@gmail.com>
2025-11-09 23:58:05 +0100

    Début du travail sur PROSIT2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,52 @@
 
 public class Main {
     public static void main(String[] args) {
+         zoo zoo1 = new zoo("Zoo de Tunis", "Tunis");
+          zoo zoo2 = new zoo("Zoo de Sousse", "Sousse");
+
         Animal lion = new Animal("Felidae", "Lion", 5, true);
         Animal elephant = new Animal("Elephantidae", "Éléphant", 10, true);
         Animal crocodile = new Animal("Crocodylidae", "Crocodile", 7, false);
-        zoo myZoo = new zoo("MyZoo", "Tunis", 20);
-        myZoo.ajouterAnimal(lion, 0);
-        System.out.println("Informations des animaux :");
+        Animal lion2 = new Animal("Felidae", "Lion", 5, true);
+        Animal tigre = new Animal("Felidae", "Tigre", 4, true);
+
+        zoo1.addAnimal(lion);
+        zoo1.addAnimal(tigre);
+        zoo2.addAnimal(crocodile);
+
+        // Vérifier si zoo1 est plein
+        System.out.println("Le zoo1 est plein ? " + zoo1.isFull());
+
+        // Comparer les deux zoos
+        zoo plusGrand = zoo1.compareZoo(zoo2);
+        System.out.println("Le zoo avec le plus d’animaux est : " + plusGrand.name);
+    }
+}
+        /*zoo2.addAnimal(crocodile);
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(elephant);
+        myZoo.addAnimal(crocodile);
+        System.out.println("\n--- Affichage du zoo ---");
+        System.out.println(myZoo);
+        myZoo.displayAnimals();
+     /*
+        System.out.println("\nAvant suppression :");
+        myZoo.displayAnimals();
+
+        myZoo.removeAnimal("Éléphant");
+
+        System.out.println("\nAprès suppression :");
+        myZoo.displayAnimals();
+
+
+        myZoo.removeAnimal("Tigre");
+        /*System.out.println();
+        myZoo.displayAnimals();*/
+
+
+
+        /*myZoo.ajouterAnimal(lion, 0);*/
+       /* System.out.println("Informations des animaux :");
         lion.afficherInfo();
         System.out.println();
         elephant.afficherInfo();
@@ -22,18 +62,35 @@ public class Main {
         System.out.println(myZoo);  // Appelle automatiquement myZoo.toString()
         System.out.println();
         System.out.println("Affichage avec myZoo.toString() :");
-        System.out.println(myZoo.toString());
+        System.out.println(myZoo.toString());*/
 
-        myZoo.ajouterAnimal(lion, 0);
-        myZoo.ajouterAnimal(elephant, 1);
-        myZoo.ajouterAnimal(crocodile, 2);
+        /*if (myZoo.addAnimal(lion)) System.out.println(lion.name + " ajouté avec succès !");
+        if (myZoo.addAnimal(elephant)) System.out.println(elephant.name + " ajouté avec succès !");
+        if (myZoo.addAnimal(crocodile)) System.out.println(crocodile.name + " ajouté avec succès !");
 
+        // Essayer d'ajouter plus d'animaux que la capacité maximale
+        for (int i = 0; i < 30; i++) {
+            Animal a = new Animal("TestFamily", "Animal" + i, i, true);
+            if (!myZoo.addAnimal(a)) {
+                System.out.println(a.name + " n'a pas pu être ajouté : zoo plein !");
+            }
+        }
 
-        System.out.println("Affichage d'un animal :");
-        System.out.println(lion);
-        System.out.println();
-
-        System.out.println("Affichage du zoo :");
+        System.out.println("\nÉtat final du zoo :");
         System.out.println(myZoo);
-    }
-}
+
+
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(elephant);
+        myZoo.addAnimal(crocodile);
+
+        // Affichage de tous les animaux
+        myZoo.displayAnimals();}}*/
+
+      /*
+        int indexLion = myZoo.findAnimalByName("Lion");
+        System.out.println("Indice de Lion : " + indexLion);
+        int indexLionRecherche = myZoo.findAnimalByName("Lion");
+        System.out.println("Indice de Lion recherché : " + indexLionRecherche);
+    }}*/
+
--- src/zoo.java
+++ src/zoo.java
@@ -1,21 +1,92 @@
 public class zoo {
-    Animal[] animals;
+    final int NBR_CAGES = 25;
+    Animal[] animals=new Animal[NBR_CAGES];
     String name;
     String city;
-    int nbrCages;
-    public zoo(String name,String city,int nbrCages){
+    int animalCount;
+    /*int nbrCages;
+    int animalCount = 0;
+    int capacity;*/
+    public zoo(String name,String city) {
        this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
-        this.animals = new Animal[25];
+       /* this.nbrCages = nbrCages;
+        this.capacity = capacity;
+        this.animals = new Animal[capacity];*/
+        this.animalCount = 0;
     }
-    public void ajouterAnimal(Animal animal, int index) {
-        if (index >= 0 && index < animals.length) {
-            animals[index] = animal;
-        } else {
-            System.out.println("Index invalide pour ajouter l'animal.");
+    public boolean addAnimal(Animal animal) {
+        if (animalCount >= NBR_CAGES) {
+            System.out.println(" Impossible d'ajouter " + animal.name + " : zoo plein (" + NBR_CAGES + " animaux max).");
+           return false;
+        }
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i].name.equals(animal.name) && animals[i].family.equals(animal.family)) {
+                System.out.println(" " + animal.name + " existe déjà dans le zoo !");
+                return false;
+            }
+        }
+        animals[animalCount] = animal;
+        animalCount++;
+        System.out.println(" " + animal.name + " ajouté avec succès !");
+        return true;
+    }
+    public void displayAnimals() {
+        if (animalCount == 0) {
+            System.out.println("Aucun animal dans le zoo.");
+            return;
+        }
+        System.out.println("Animaux présents dans le zoo :");
+        for (int i = 0; i < animalCount; i++) {
+            System.out.println("- " + animals[i]);
         }
     }
+    //  Vérifier si le zoo est plein
+    public boolean isFull() {
+        return animalCount >= NBR_CAGES;
+    }
+    //  Comparer deux zoos
+    public zoo compareZoo(zoo z) {
+        if (this.animalCount > z.animalCount) {
+            return this;
+        } else {
+            return z;
+        }
+    }
+
+    // Supprimer un animal par nom
+public boolean removeAnimal(String name) {
+    for (int i = 0; i < animalCount; i++) {
+        if (animals[i].name.equals(name)) {
+            // Décaler les éléments suivants
+            for (int j = i; j < animalCount - 1; j++) {
+                animals[j] = animals[j + 1];
+            }
+            animals[animalCount - 1] = null; // dernière case devient vide
+            animalCount--; // décrémente le compteur
+            System.out.println(" " + name + " supprimé du zoo.");
+            return true;
+        }
+    }
+    System.out.println("Animal " + name + " introuvable.");
+    return false;
+}
+    /*
+    public int findAnimalByName(String name) {
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i].name.equals(name)) {
+                return i;
+            }
+        }
+        return -1; // non trouvé
+    }*/
+    @Override
+    public String toString() {
+        return "Zoo : " + name + " (" + city + "), Capacité : " + NBR_CAGES + " cages, " +
+                "Animaux présents : " + animalCount;
+    }
+}
+   /*
     @Override
     public String toString() {
         String info = "Nom du zoo : " + name +
@@ -28,7 +99,8 @@ public class zoo {
             }
         }
         return info;
-    }
+    }}*/
+    /*
     public void displayZoo() {
         System.out.println("Nom du zoo : " + name);
         System.out.println("Ville : " + city);
@@ -43,5 +115,5 @@ public class zoo {
             if (a != null) {
                 System.out.println("- " + a.name + " (" + a.family + ")");
             }
-        }
-}}
+        }*/
+
