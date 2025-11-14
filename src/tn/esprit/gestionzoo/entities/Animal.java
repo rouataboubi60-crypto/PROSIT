d30d9332bcad9dcc54651e59b7d3f5ca3536e4e0
taboubiroua <rouataboubi60@gmail.com>
2025-11-14 22:27:31 +0100

    modifier

--- src/tn/esprit/gestionzoo/entities/Animal.java
+++ src/tn/esprit/gestionzoo/entities/Animal.java
@@ -5,6 +5,8 @@ public class Animal {
     private String name;
     private int age;
     private boolean isMammal;
+    public Animal() {
+    }
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
---
+++ src/tn/esprit/gestionzoo/entities/Aquatic.java
@@ -0,0 +1,5 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Aquatic extends Animal {
+    protected String habitat;
+}
